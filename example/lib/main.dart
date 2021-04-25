54f349579c4f24834794fb0b040fd48275092890
Yongze.chen <sapphire.php@gmail.com>
2021-04-25 09:03:01 +0800

    up

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,88 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:xgk_package/abc.dart';
-import 'package:xgk_package/xgk_package.dart';
+import './views/index.dart';
+import './router/index.dart';
+import 'package:xgk_package/weui.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  AppState createState() => AppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+class AppState extends State {
+  bool isDefaultTheme = true;
+
+  void toggleTheme() {
+    this.setState(() {
+      isDefaultTheme = !isDefaultTheme;
+    });
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          Abc(),
-          Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
-            child: XgkPackage(
-              text: 'Shaded Text',
-              textColor: Color(0xffff0000),
-              shadeColor: Color(0xff00ff00),
-              shadeBuilder: (BuildContext context, String text, Color color) =>
-                  Container(
-                child: Text(
-                  text,
-                  style: TextStyle(color: color),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return WeUi(
+        theme: isDefaultTheme
+            ? WeTheme()
+            : WeTheme(
+                primaryColor: Color(0xff1989fa),
+                primaryColorDisabled: Color(0xff5cadff),
+                warnColor: Color(0xfffb4343),
+                warnColorDisabled: Color(0xfffaa7a3)),
+        config: WeConfig(toastSuccessDuration: 5000),
+        child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            routes: routes,
+            home: Index(this.toggleTheme)));
   }
 }
--- test/xgk_package_test.dart
+++ test/xgk_package_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xgk_package/xgk_package.dart';
+import 'package:xgk_package/weui.dart';
 
 void main() {
   test('adds one to input values', () {
